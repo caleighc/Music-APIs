0c58854cb8c1aebe427c193ca147183934de71c4
Caleigh Crossman <caleighcrossman@Caleighs-MacBook-Pro.local>
2022-12-05 12:17:56 -0500

    database

--- spotify.py
+++ spotify.py
@@ -3,10 +3,16 @@ import sqlite3
 import json
 import os
 import matplotlib.pyplot as plt
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
 
 # Caleigh Crossman
 # Spotify API 
 
-import math 
 
-
+# Set up the database given the db_name
+def set_up_db(db_name):
+    path = os.path.dirname(os.path.abspath(__file__))
+    conn = sqlite3.connect(path+'/'+db_name)
+    cur = conn.cursor()
+    return cur, conn
