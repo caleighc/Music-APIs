b7897fc78a0e20fa6d42d0b2b81506dfd9091e36
Caleigh Crossman <caleighcrossman@Caleighs-MacBook-Pro.local>
2022-12-10 17:12:03 -0500

    main

--- main.py
+++ main.py
@@ -28,14 +28,20 @@ def main():
         spotify.add_songs(jsonDict,cur,conn)
     # Make calculations and visualizations for spotify 
     #spotify.make_visualizations(cur,conn)
-    spotify.artists_visualization(cur,conn)
+    #spotify.artists_visualization(cur,conn)
     # Create tables for genius
     #genius.song_functions(cur,conn)
     # Add data for genius 
 
-    # Create tables for apple music 
-    
-    # Add data for apple music 
+
+    artist_id =  ['20244d07-534f-4eff-b4d4-930878889970', '9fff2f8a-21e6-47de-a2b8-7f449929d43f', '381086ea-f511-4aba-bdf9-71c753dc5077', '65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab', 'c8b03190-306c-4120-bb0b-6f2ebfc06ea9',
+    '859d0860-d480-4efd-970c-c05d5f1776b8', 'f4fdbb4c-e4b7-47a0-b83b-d91bbfcfa387', 'f27ec8db-af05-4f36-916e-3d57f91ecf5e', '73e5e69d-3554-40d8-8516-00cb38737a1c', 'b8a7c51f-362c-4dcb-a259-bc6e0095f0a6']
+    for id in artist_id:
+        audio.request_data(id)
+    # Create tables for audio-db 
+    audio.create_table(cur, conn)
+    # Add data for audio-db
+    audio.insert_song_id(cur, conn)
 
 if __name__ == "__main__":
     main()
