06f095cf5e452e73b9c3a7a68a7a409603ea9107
Remi Goldfarb <remigoldfarb>
2022-12-11 16:03:48 -0500

    third

--- genius.py
+++ genius.py
@@ -13,16 +13,29 @@ API_KEY = "HAQwWUcRzMqn3cp-AkOLU0WbsB9bqn4tJ56O_kg8ykbLyYMFoqDgUJ6oxt0MBsuY"
 from lyricsgenius import Genius
 genius = Genius(API_KEY)
 
+list_1 = []
+list_2 = []
+list_3 = []
+list_4 = []
+
 def song_functions():
   artist = genius.search_artist("SZA", max_songs = 25, sort = 'popularity')
-  print(artist.songs)
+  #print(artist.songs)
+  list_1.append(artist.songs)
+  artist.save_lyrics()
   artist = genius.search_artist("A Boogie wit da Hoodie", max_songs = 25, sort = 'popularity')
-  print(artist.songs)
+  #print(artist.songs)
+  list_2.append(artist.songs)
+  artist.save_lyrics()
   artist = genius.search_artist("Lana Del Rey", max_songs = 25, sort = 'popularity')
-  print(artist.songs)
+  #print(artist.songs)
+  list_3.append(artist.songs)
+  artist.save_lyrics()
   artist = genius.search_artist("Taylor Swift", max_songs = 25, sort = 'popularity')
-  print(artist.songs)
-  return artist.songs
+  #print(artist.songs)
+  list_4.append(artist.songs)
+  artist.save_lyrics()
+  #return artist.songs
     
   
 #artist = genius.search_artist("Drake", max_songs = 100, sort = 'popularity')
@@ -30,25 +43,27 @@ def song_functions():
 
 # create table
 def create_tables(cur,conn):
-  cur.execute('DROP TABLE IF EXISTS Song')
-  cur.execute('CREATE TABLE Song (Song.title TEXT PRIMARY KEY, Song.artist TEXT, Song.year)')
+  cur.execute('DROP TABLE IF EXISTS SongsAudiodb')
+  cur.execute("CREATE TABLE IF NOT EXISTS SongsAudiodb ((full_title TEXT PRIMARY KEY, name TEXT, year INT)")
   conn.commit()
+
 
   
 # insert data into table 
 def add_data(cur, conn):
-    for item in range(len(Song.title)):
-        song_column = Song.title[item]
-        artist_column = Song.artist[item]
-        year_column = Song.year[item]
-        cur.execute("INSERT OR IGNORE INTO Song (Song.title, Song.artist, Song.artist) VALUES (?, ?, ?)", (Song.title, Song.artist, Song.year))
-    conn.commit()
-  #cur.execute("INSERT INTO music_name_table (song,id,artist,popularity) VALUES (?,?,?,?)",(song_name,song_id,artist_id,popularity))
- 
-  #conn.commit()
+  print(list_1)
+  for item in range(len(full_title)):
+    title_column = full_title[item]
+    name_column = name[item]
+    year_column = year[item]
+    cur.execute("INSERT OR IGNORE INTO SongsAudiodb (full_title, name, year) VALUES (?, ?, ?)", (title_column, name_column, year_column))
+    #conn.commit()
+
+
   #if i decide to put parameters in just update both song_functions()
   #find one of the sql functions that inserts info into the database
   
 #find average length of the title and compare it to popularity
 
 song_functions()
+add_data(cur, conn)
