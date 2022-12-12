bfa8b96a75f9f93a1666aea4012bac9c4f4137b2
avaweb <73961996+avaweb@users.noreply.github.com>
2022-12-12 14:07:39 -0500

    Add files via upload

--- audio.py
+++ audio.py
@@ -100,10 +100,8 @@ def request_data(artist_ids):
 
 # create table
 def create_table(cur, conn):
-    cur.execute('DROP TABLE IF EXISTS SongsAudiodb')
     cur.execute("CREATE TABLE IF NOT EXISTS SongsAudiodb (song_id INT PRIMARY KEY, song TEXT, artist_id INT, score TEXT, genre TEXT)")
 
-    cur.execute('DROP TABLE IF EXISTS ArtistsAudiodb')
     cur.execute("CREATE TABLE IF NOT EXISTS ArtistsAudiodb (artist_id INT PRIMARY KEY, artist TEXT)")
 
     conn.commit()
@@ -226,16 +224,3 @@ def make_chart2():
     plt.axis('equal')
     plt.title("Percentage of Songs in Each Genre")
     plt.show() 
-
-
-def main():
-
-    artist_ids = ['20244d07-534f-4eff-b4d4-930878889970', '9fff2f8a-21e6-47de-a2b8-7f449929d43f', '381086ea-f511-4aba-bdf9-71c753dc5077', '65f4f0c5-ef9e-490c-aee3-909e7ae6b2ab', 'c8b03190-306c-4120-bb0b-6f2ebfc06ea9',
-    'e0140a67-e4d1-4f13-8a01-364355bee46e', 'f4fdbb4c-e4b7-47a0-b83b-d91bbfcfa387', 'f27ec8db-af05-4f36-916e-3d57f91ecf5e', '73e5e69d-3554-40d8-8516-00cb38737a1c', 'b8a7c51f-362c-4dcb-a259-bc6e0095f0a6']
-    for id in artist_ids:
-        request_data(id)
-    
-
-
-if __name__ == "__main__":
-    main()
