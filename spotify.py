dd58dc25cae20fe660a6081cad9105ac2a6e4ddc
Caleigh Crossman <caleighcrossman@Caleighs-MacBook-Pro.local>
2022-12-12 17:40:21 -0500

    deletion

--- spotify.py
+++ spotify.py
@@ -68,15 +68,6 @@ def add_songs(data,cur,conn):
         cur.execute("INSERT OR IGNORE INTO Songs (song_name,song_id,artist_id,popularity,valence,danceability,energy) VALUES (?,?,?,?,?,?,?)",(song_name,song_id,artist_id,popularity,valence,danceability,energy))
     conn.commit()
 
-# Reads and loads the json data from the file
-def read_data(filename):
-    full_path = os.path.join(os.path.dirname(__file__), filename)
-    f = open(full_path)
-    file_data = f.read()
-    f.close()
-    json_data = json.loads(file_data)
-    return json_data
-
 # Write the json data to a file 
 def write_json(filename,dict):
     jsonString = json.dumps(dict)
