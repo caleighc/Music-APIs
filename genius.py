94098e080d60d74d0aedbcf4d603490914745ef4
avaweb <73961996+avaweb@users.noreply.github.com>
2022-12-12 09:11:54 -0500

    Update genius.py

--- genius.py
+++ genius.py
@@ -84,11 +84,11 @@ def add_data_1(cur, conn):
   f.close()
   data_2 = json.loads(data)
   for item in range(0, 25):
-    title_column = data_2["songs"][0]["full_title"]
+    title_column = data_2["songs"][item]["full_title"]
     name_column = data_2["name"]
-    year_column = data_2["songs"][0]["release_date_components"]["year"]
+    year_column = data_2["songs"][item]["release_date_components"]["year"]
     cur.execute("INSERT or IGNORE INTO Genius (full_title, name, year) VALUES (?, ?, ?)", (title_column, name_column, year_column))
-    #conn.commit()
+  conn.commit()
 
 def add_data_2(cur, conn): 
   f = open("Lyrics_LanaDelRey.json")
@@ -96,11 +96,11 @@ def add_data_2(cur, conn):
   f.close()
   data_3 = json.loads(data)
   for item in range(25, 50):
-    title_column = data_3["songs"][0]["full_title"]
+    title_column = data_3["songs"][item]["full_title"]
     name_column = data_3["name"]
-    year_column = data_3["songs"][0]["release_date_components"]["year"]
+    year_column = data_3["songs"][item]["release_date_components"]["year"]
     cur.execute("INSERT or IGNORE INTO Genius (full_title, name, year) VALUES (?, ?, ?)", (title_column, name_column, year_column))
-  #conn.commit()
+  conn.commit()
 
 def add_data_3(cur, conn): 
   f = open("Lyrics_ABoogiewitdaHoodie.json")
@@ -108,11 +108,11 @@ def add_data_3(cur, conn):
   f.close()
   data_4 = json.loads(data)
   for item in range(50, 75):
-    title_column = data_4["songs"][0]["full_title"]
+    title_column = data_4["songs"][item]["full_title"]
     name_column = data_4["name"]
-    year_column = data_4["songs"][0]["release_date_components"]["year"]
+    year_column = data_4["songs"][item]["release_date_components"]["year"]
     cur.execute("INSERT or IGNORE INTO Genius (full_title, name, year) VALUES (?, ?, ?)", (title_column, name_column, year_column))
-    #conn.commit()
+  conn.commit()
 
 def add_data_4(cur, conn): 
   f = open("Lyrics_TaylorSwift.json")
@@ -120,11 +120,11 @@ def add_data_4(cur, conn):
   f.close()
   data_5 = json.loads(data)
   for item in range(75, 100):
-    title_column = data_5["songs"][0]["full_title"]
+    title_column = data_5["songs"][item]["full_title"]
     name_column = data_5["name"]
-    year_column = data_5["songs"][0]["release_date_components"]["year"]
+    year_column = data_5["songs"][item]["release_date_components"]["year"]
     cur.execute("INSERT or IGNORE INTO Genius (full_title, name, year) VALUES (?, ?, ?)", (title_column, name_column, year_column))
-    #conn.commit()
+  conn.commit()
 
   #print(list_1)
   #for item in range(len("full_title")):
